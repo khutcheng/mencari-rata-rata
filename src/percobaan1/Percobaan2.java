0e69a1e56d3f6aa91f9e22311f6624aaf89d98fc
khutcheng <ksari5854@gmail.com>
2026-04-22 19:00:12 +0700

    menambahkan data(nama,no telepon,alamat)

--- src/percobaan1/Percobaan2.java
+++ src/percobaan1/Percobaan2.java
@@ -9,5 +9,10 @@ package percobaan1;
  * @author rizal
  */
 public class Percobaan2 {
-    
+    String data [][] = {
+            {"ABDUL", "085646668991", "KEDIRI"},
+            {"KUSNO", "085646668992", "TRENGGALEK"},
+            {"PONIRAN", "085646668999", "BOJONEGORO"},
+            {"KUCENG", "083897271987", "NGANJUK"}
+    }
 }
