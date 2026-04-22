fe48e3dc4463154e95b82c3206843b304f1e1170
khutcheng <ksari5854@gmail.com>
2026-04-22 16:09:30 +0700

    menampilkan angka

--- src/percobaan1/Percobaan1.java
+++ src/percobaan1/Percobaan1.java
@@ -15,6 +15,14 @@ public class Percobaan1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int data[][] = {{4, 6, 4, 2, 8, 4, 2, 10}, {4, 6, 4, 2, 8, 4, 
+    2, 10}}; 
+        for (int[] data1 :data){
+            for (int b   = 0; b < data[0].length; b++) {
+                System.out.print(data1[b] + "");
+            }
+            System.out.println();
+        } 
     }
     
 }
